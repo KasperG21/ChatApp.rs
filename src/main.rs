c3b279416368b3af61c74f9c8c0f460d73ae94b0
Kasper_ <kasper.goddeau@gmail.com>
2024-09-17 19:42:07 +0200

    First changes, basic layout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
+use gloo_console::log;
 use yew::prelude::*;
+
+struct App {}
+
+enum Msg {}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+       Self {}
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        todo!();
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div id="app">
+                <h1 class={classes!("titles")}>{"ChatApp.rs"}</h1>
+                <div id={"mainInputContainer"}>
+                    <input id={"mainTextInput"} class={classes!("mainInputs")} />
+                    <button id={"mainButtonInput"} class={classes!("mainInputs")}>{"SEND"}</button>
+                </div>
+            </div>
+        }
+    }
+}
 
 fn main() {
     yew::Renderer::<App>::new().render();
-}
-
-#[function_component]
-fn App() -> Html {
-    html! {
-        <h1 class="titles">{"Hello, World!"}</h1>
-    }
 }
